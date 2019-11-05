3e7860781ff5c0a4e96a6cb2e5b8c7f1b9a1754d
Xi Sun <xixihappycc@gmail.com>
2019-11-05 11:01:13 -0800

    unity slippy map support version

---
+++ Assets/Scripts/MapLockButton.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TouchScript;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class MapLockButton : MonoBehaviour, IPointerClickHandler
+{
+    // Start is called before the first frame update
+    public Sprite[] sprites;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.Find("Test").GetComponent<TestMap>().isMapRotateLocked)    
+            this.GetComponent<Image>().sprite = sprites[1];      
+        else
+            this.GetComponent<Image>().sprite = sprites[0];
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+
+        if (eventData.pointerCurrentRaycast.gameObject != null)
+        {
+            if (GameObject.Find("Test").GetComponent<TestMap>().isMapRotateLocked)
+            {
+                this.GetComponent<Image>().sprite = sprites[0];
+                GameObject.Find("Test").GetComponent<TestMap>().isMapRotateLocked = false;
+            }
+            else
+            {
+                this.GetComponent<Image>().sprite = sprites[1];
+                GameObject.Find("Test").GetComponent<TestMap>().isMapRotateLocked = true;
+            }
+        }
+       
+            
+    }
+
+}
